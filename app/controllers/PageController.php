862bdd965e11556016278e7567beab513c73cc26
wasabireal <shulgach2013@gmail.com>
2023-10-06 18:06:33 +0300

    PageController created
    Signed-off-by: wasabireal <shulgach2013@gmail.com>

---
+++ app/controllers/PageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\controllers;
+
+use wsb\App;
+use app\models\Page;
+
+/** @property Page $model */
+class PageController extends AppController
+{
+
+    public function viewAction()
+    {
+
+        $lang = App::$app->getProperty('language');
+        $page = $this->model->get_page($this->route['slug'], $lang);
+
+        if(!$page){
+            $this->error404();
+            return;
+        }
+
+        $this->setMeta($page['title'], $page['description'], $page['keywords']);
+        $this->set(compact('page'));
+
+    }
+
+}
---
+++ app/views/Page/view.php
@@ -0,0 +1,24 @@
+<?php
+/** @var $page array */
+?>
+<div class="container">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb bg-light p-2">
+            <li class="breadcrumb-item"><a href="<?= base_url() ?>"><i class="fas fa-home"></i></a></li>
+            <li class="breadcrumb-item"><?= $page['title'] ?></li>
+        </ol>
+    </nav>
+</div>
+
+<div class="container py-3">
+    <div class="row">
+
+        <div class="col-lg-12 category-content">
+            <h1 class="section-title"><?= $page['title'] ?></h1>
+
+            <?= $page['content'] ?>
+
+        </div>
+
+    </div>
+</div>
